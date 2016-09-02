78d35fca9a75b9fff6b99f129336b45ace47bd81
CBrooks12 <cbrooks4@uoregon.edu>
2016-09-02 11:28:07 -0700

    first commit

---
+++ MapEdit.js
@@ -0,0 +1,59 @@
+var map;
+
+function initMap() {
+	var styleArray = [
+	 {
+		featureType: "road",
+		elementType: "labels",
+		stylers: [
+		  { visibility: "off" }
+		]
+	  }
+	  ,{
+		featureType: "poi",
+		elementType: "all",
+		stylers: [
+		  { visibility: "off" }
+		]
+	  }
+	  ,{
+		featureType: "water",
+		stylers: [
+		  { "color": "#161616" }
+		]
+	  }
+	  ,{
+		featureType: "landscape",
+		stylers: [
+		  {"color": "#141414"}
+		]
+	  }
+	  ,{
+		featureType: "administrative",
+		stylers: [
+		  { visibility: "off" }
+		]
+	  },{
+		featureType: "administrative.locality",
+		stylers: [
+		  { visibility: "on" }
+		]
+	  }/**,{
+		featureType: "landscape.natural.terrain",
+		elementType: "all",
+		stylers: [
+		  {"color": "#222222"}
+		]
+	  }**/
+	];
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 44.052,
+      lng: -123.086
+    },
+	styles: styleArray,
+    zoom: 12
+  });
+	//var trafficLayer = new google.maps.TrafficLayer();
+	//trafficLayer.setMap(map);
+}
